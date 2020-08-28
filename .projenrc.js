089850812c6bec3b885acc63a254ed4b983d6cfa
Pahud <pahudnet@gmail.com>
2020-08-28 20:12:14 +0800

    bump cdk 1.61.1

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const {
   ConstructLibraryAws,
 } = require('projen');
 
-const LAST_AWSCDK_VERSION = '1.61.0';
+const LAST_AWSCDK_VERSION = '1.61.1';
 
 const project = new ConstructLibraryAws({
   "authorName": "Pahud Hsieh",
