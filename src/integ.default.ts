087c8a0d913d7cdb8dfc40c86d58359b60636c59
Pahud <pahudnet@gmail.com>
2020-09-09 12:36:13 +0800

    chore(test): update tests

---
+++ src/integ.default.ts
@@ -0,0 +1,48 @@
+import { ServerlessLaravel, DatabaseCluster } from './index';
+import * as cdk from '@aws-cdk/core';
+import { Vpc, InstanceType } from '@aws-cdk/aws-ec2';
+import * as path from 'path';
+
+export class IntegTesting {
+  readonly stack: cdk.Stack[];
+
+  constructor() {
+  const app = new cdk.App();
+  const env = {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  };
+
+  const stack = new cdk.Stack(app, 'testing-stack', { env });
+
+  const vpc = new Vpc(stack, 'Vpc', { maxAzs: 3, natGateways: 1 });
+
+  // the DatabaseCluster sharing the same vpc with the ServerlessLaravel
+  const db = new DatabaseCluster(stack, 'DatabaseCluster', {
+    vpc,
+    instanceType: new InstanceType('t3.small'),
+    rdsProxy: true,
+    instanceCapacity: 1,
+  })
+
+  // the ServerlessLaravel
+  new ServerlessLaravel(stack, 'ServerlessLaravel', {
+    brefLayerVersion: 'arn:aws:lambda:ap-northeast-1:209497400698:layer:php-74-fpm:11',
+    laravelPath: path.join(__dirname, '../codebase'),
+    vpc,
+    databaseConfig: {
+      writerEndpoint: db.rdsProxy!.endpoint,
+    },
+  });
+
+  new cdk.CfnOutput(stack, 'RDSProxyEndpoint', { value: db.rdsProxy!.endpoint })
+  new cdk.CfnOutput(stack, 'DBMasterUser', { value: db.masterUser })
+  new cdk.CfnOutput(stack, 'DBMasterPasswordSecret', { value: db.masterPassword.secretArn })
+  
+  this.stack = [ stack ]
+  }
+}
+
+
+// run the integ testing
+new IntegTesting();
--- test/integ.api.test.ts
+++ test/integ.api.test.ts
@@ -1,48 +1,10 @@
-import { ServerlessLaravel, DatabaseCluster } from '../src';
+import '@aws-cdk/assert/jest';
+import { IntegTesting } from '../src/integ.default';
 import { SynthUtils } from '@aws-cdk/assert';
-import { App, Stack, CfnOutput } from '@aws-cdk/core';
-import { Vpc, InstanceType } from '@aws-cdk/aws-ec2';
-import * as path from 'path';
 
-test('snapshot validation', () => {
-  const app = new App();
-
-  const env = {
-    region: process.env.CDK_DEFAULT_REGION,
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-  };
-
-  const stack = new Stack(app, 'testing-stack', { env });
-
-  const vpc = new Vpc(stack, 'Vpc', { maxAzs: 3, natGateways: 1});
-
-  // the DatabaseCluster sharing the same vpc with the ServerlessLaravel
-  const db = new DatabaseCluster(stack, 'DatabaseCluster', {
-    vpc,
-    instanceType: new InstanceType('t3.small'),
-    rdsProxy: true,
-    instanceCapacity: 1,
-  })
-
-  // the ServerlessLaravel
-  new ServerlessLaravel(stack, 'ServerlessLaravel', {
-    brefLayerVersion: 'arn:aws:lambda:ap-northeast-1:209497400698:layer:php-74-fpm:11',
-    laravelPath: path.join(__dirname, '../codebase'),
-    vpc,
-    databaseConfig: {
-      writerEndpoint: db.rdsProxy!.endpoint,
-    },
+test('integ snapshot validation', () => {
+  const integ = new IntegTesting();
+  integ.stack.forEach(stack => {
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
   });
-
-  new CfnOutput(stack, 'RDSProxyEndpoint', { value: db.rdsProxy!.endpoint })
-  new CfnOutput(stack, 'DBMasterUser', { value: db.masterUser })
-  new CfnOutput(stack, 'DBMasterPasswordSecret', { value: db.masterPassword.secretArn })
-  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-});
-
-
-
-
-
-
-
+})
